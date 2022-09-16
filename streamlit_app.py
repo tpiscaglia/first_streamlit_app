9f2d3228513483efba54bd285f199a0def2947f4
Tyler Piscaglia <61087490+tpiscaglia@users.noreply.github.com>
2022-09-16 13:09:56 -0500

    Update streamlit_app.py
    Update multiselect to show fruit names instead of indexes.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit
 import pandas
 
-my_fruits_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -12,6 +10,9 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruits_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruits_list = my_fruits_list.set_index('Fruit')
 
 #using a pick list to let the user select their fruits
 streamlit.multiselect('Pick some fruits: ', list(my_fruits_list.index))
